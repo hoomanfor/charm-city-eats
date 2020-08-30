6a969e3f72ea1f730fa95aecab5b9794ca9c7327
Hoo Man <hoomanfor@gmail.com>
2020-08-30 13:53:07 -0400

    renamed environmental variables

--- config/config.js
+++ config/config.js
@@ -1,18 +1,11 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": process.env.SEQUELIZE_USER,
-    "password": process.env.SEQUELIZE_PASSWORD,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "city_eats_db",
     "host": "localhost",
-    "port": 3306,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "production": {
