dee44090f8e54d1db05af44aff807e6dcdadedab
Hoo Man <hoomanfor@gmail.com>
2020-09-01 21:38:47 -0400

    modal form allows users to edit restaurant name

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const matchList = $("#match-list");
 
 // Search Restaurants and filter it
 const searchRestaurants = async (searchText) => {
+  $(".alert").hide()
   const response = await fetch("/api/restaurants");
   const restaurants = await response.json();
 
@@ -18,7 +19,6 @@ const searchRestaurants = async (searchText) => {
     matchList.html("");
   };
 
-  // console.log(matches);
   outputHtml(matches);
 };
 
@@ -26,30 +26,32 @@ const searchRestaurants = async (searchText) => {
 const outputHtml = matches => {
   if (matches.length > 0) {
     const html = matches.map(match => `
-      <div class="card border-primary mb-1">
-      <div class="card-header">${match.address} Baltimore, MD ${match.zip_code}</div>
-      <div class="card-body">
-        <h4 class="card-title">${match.name}</h4>
-        <p class="card-text">${match.neighborhood}</p>
+      <div class="card border-primary mb-2">
+        <div class="card-header">
+          ${match.address} Baltimore, MD ${match.zip_code}
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">${match.name}</h4>
+          <p class="card-text">${match.neighborhood}</p>
+          <button type="button" id="update" data-id=${match.id} data-toggle="modal" data-target="#update-restaurant-name" class="btn btn-primary w-100 mb-2">Update restaurant name</button>
+          <button type="button" id="delete" data-id=${match.id} class="btn btn-warning w-100">Report restaurant as closed</button>
         </div>
       </div>
     `)
     .join("");
 
-    // console.log(html)
     matchList.html(html);
   };
 };
 
 // Event listener for restaurant search
-$(document).on("input", () => searchRestaurants(search.val()));
+$("#search").on("input", () => searchRestaurants(search.val()));
 
 // Add a new restaurant to the database
-$(document).on("submit", function(event) {
+$("#create-submit").on("click", function(event) {
   event.preventDefault();
   const restaurantForm = $("#restaurant-form")[0];
   if (restaurantForm.checkValidity() === false) {
-    event.preventDefault();
     event.stopPropagation();
     $("#restaurant-form").addClass("was-validated");
   } else {
@@ -63,15 +65,41 @@ $(document).on("submit", function(event) {
       neighborhood,
       zip_code
     };
-    console.log(restaurant)
     $.ajax({
       url: "/api/restaurants",
       data: restaurant,
       dataType: "json",
       method: "POST"
     }).then(function(response) {
+      $(".alert").show()
       $('#add-restaurant-form').modal('toggle')
       $("#restaurant-form").removeClass("was-validated");
     })
+  };
+});
+
+$("#update-submit").on("click", function(event) {
+  event.preventDefault();
+  const id = $("#update").data("id");
+  const updateForm = $("#update-restaurant-form")[0];
+  if (updateForm.checkValidity() == false) {
+    event.stopPropagation();
+    $("#update-restaurant-form").addClass("was-validated");
+  } else {
+    const newName = $("#updateName").val().trim();
+    $.ajax({
+      url: "/api/restaurants",
+      data: { 
+        "id": id,
+        "name": newName
+      },
+      dataType: "json",
+      method: "PUT"
+    }).then(function(response) {
+      $(".alert").show()
+      $('#update-restaurant-name').modal('toggle')
+      $("#update-restaurant-form").removeClass("was-validated");
+      matchList.html("");
+    });
   };
 });
